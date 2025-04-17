696889e90495b34e8ae8353a6e4b12f73f5c2767
Lee Jung Ho <117253154+Jarabro@users.noreply.github.com>
2025-04-17 09:58:34 +0900

    Update chat_clnt.c

--- chat_clnt.c
+++ chat_clnt.c
@@ -7,18 +7,23 @@
 #include <pthread.h>
 
 #define BUF_SIZE 100
+#define NAME_SIZE 20
 
+void * send_msg(void * arg);
+void * recv_msg(void * arg);
 void error_handling(char * msg);
 
-int main(int argc, char *argv[])
-{
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
+
+int main(int argc, char *argv[]) {
     int sock;
     struct sockaddr_in serv_addr;
     char msg[BUF_SIZE];
     int str_len;
 
-    if (argc != 4) {
-        printf("Usage : %s <IP> <Port> <Nickname>\n", argv[0]);
+    if(argc != 3) {
+        printf("Usage : %s <IP> <port> <name>\n", argv[0]);
         exit(1);
     }
 
@@ -28,66 +33,53 @@ int main(int argc, char *argv[])
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
 
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
         error_handling("connect() error");
 
-    // 자동으로 닉네임 설정
-    write(sock, "2\n", strlen("2\n"));
-    str_len = read(sock, msg, BUF_SIZE - 1); msg[str_len] = 0;
-    printf("%s", msg);
-    write(sock, argv[3], strlen(argv[3]));
-    str_len = read(sock, msg, BUF_SIZE - 1); msg[str_len] = 0;
-    printf("%s", msg);
+    pthread_t recv_thread;
+    pthread_create(&recv_thread, NULL, recv_msg, (void *)&sock);
+    pthread_detach(recv_thread);
 
-    while (1)
-    {
+    while(1) {
+        // 서버로부터 메뉴를 읽어서 출력
         str_len = read(sock, msg, BUF_SIZE - 1);
-        if (str_len <= 0) break;
+        if(str_len <= 0) break;
         msg[str_len] = 0;
         printf("%s", msg);
 
+        // 사용자 입력 전송
         fgets(msg, BUF_SIZE, stdin);
         write(sock, msg, strlen(msg));
 
-        if (strncmp(msg, "1", 1) == 0) {
-            while (1) {
+        if(strncmp(msg, "1", 1) == 0) {
+            // 채팅방 입장
+            while(1) {
                 str_len = read(sock, msg, BUF_SIZE - 1);
-                if (str_len <= 0) break;
+                if(str_len <= 0) break;
                 msg[str_len] = 0;
                 printf("%s", msg);
 
                 fgets(msg, BUF_SIZE, stdin);
-                write(sock, msg, strlen(msg));
-                if (strncmp(msg, "returnmenu", 10) == 0)
+                if(strncmp(msg, "returnmenu", 10) == 0) {
+                    write(sock, msg, strlen(msg));
                     break;
+                }
+                write(sock, msg, strlen(msg));
             }
         }
-        else if (strncmp(msg, "2", 1) == 0) {
-            str_len = read(sock, msg, BUF_SIZE - 1); msg[str_len] = 0;
+        else if(strncmp(msg, "2", 1) == 0) {
+            // 닉네임 변경
+            str_len = read(sock, msg, BUF_SIZE - 1);
+            if(str_len <= 0) break;
+            msg[str_len] = 0;
             printf("%s", msg);
+
             fgets(msg, BUF_SIZE, stdin);
             write(sock, msg, strlen(msg));
-            str_len = read(sock, msg, BUF_SIZE - 1); msg[str_len] = 0;
-            printf("%s", msg);
-        }
-        else if (strncmp(msg, "3", 1) == 0) {
-            break;
-        }
-        else {
+
             str_len = read(sock, msg, BUF_SIZE - 1);
-            if (str_len <= 0) break;
+            if(str_len <= 0) break;
             msg[str_len] = 0;
             printf("%s", msg);
         }
-    }
-
-    close(sock);
-    return 0;
-}
-
-void error_handling(char * msg)
-{
-    fputs(msg, stderr);
-    fputc('\n', stderr);
-    exit(1);
-}
+        else if
